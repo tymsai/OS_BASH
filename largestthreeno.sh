f4eafaf1ce4d1f0a3c8216e2c87d5d21d0d60186
T.Y.M.SAI <tymsai6076@gmail.com>
2021-10-27 15:40:02 +0530

    Create largestthreeno.sh

---
+++ largestthreeno.sh
@@ -0,0 +1,14 @@
+echo "Enter 3 numbers"
+read a
+read b
+read c
+if [ $a -gt $b && $a -gt $c ]
+    then
+        echo $a
+fi
+if [ $b -gt $a && $b -gt $c ]
+    then
+        echo $b
+    elif
+    echo $c
+fi
